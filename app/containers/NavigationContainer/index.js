11ed172f0cd5337930c654a5ae21fb0edbb5c533
Zhenghao Jia <cherie-xia@hotmail.com>
2019-12-29 21:36:19 +0800

    redux-saga

--- app/containers/NavigationContainer/index.js
+++ app/containers/NavigationContainer/index.js
@@ -11,8 +11,17 @@ import React from "react";
 import { connect } from "react-redux";
 import selectNavigationContainer from "./selectors";
 import Navigation from "../../components/Navigation";
+import { requestTopics, selectTopic } from "./actions";
 
 export class NavigationContainer extends React.Component {
+  static propTypes = {
+    requestTopics: React.PropTypes.func.isRequired
+  };
+
+  componentWillMount() {
+    this.props.requestTopics();
+  }
+
   // eslint-disable-line react/prefer-stateless-function
   render() {
     return <Navigation {...this.props} />;
@@ -23,7 +32,8 @@ const mapStateToProps = selectNavigationContainer();
 
 function mapDispatchToProps(dispatch) {
   return {
-    dispatch
+    requestTopics: () => dispatch(requestTopics()),
+    selectTopic: topic => dispatch(selectTopic(topic)) // eslint-disable-line
   };
 }
 
--- app/containers/NavigationContainer/reducer.js
+++ app/containers/NavigationContainer/reducer.js
@@ -7,29 +7,18 @@
  */
 
 import { fromJS } from "immutable";
-import { DEFAULT_ACTION } from "./constants";
+import { REQUEST_TOPICS_SUCCEEDED, SELECT_TOPIC } from "./constants";
 
 const initialState = fromJS({
-  topics: [
-    {
-      name: "libraries",
-      description: "links to useful open source libraries"
-    },
-    {
-      name: "apps",
-      description: "links to new and existing apps"
-    },
-    {
-      name: "news",
-      description: "links to progamming related news articles"
-    }
-  ]
+  topics: []
 });
 
 function navigationContainerReducer(state = initialState, action) {
   switch (action.type) {
-    case DEFAULT_ACTION:
-      return state;
+    case REQUEST_TOPICS_SUCCEEDED:
+      return state.set("topics", action.topics);
+    case SELECT_TOPIC:
+      return state.set("selectedTopic", action.topic);
     default:
       return state;
   }
