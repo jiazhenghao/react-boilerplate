8aa37e8616014119353f557bda8ca5273710bcc1
Zhenghao Jia <cherie-xia@hotmail.com>
2019-12-30 23:52:17 +0800

    small change

--- app/components/LinkList/index.js
+++ app/components/LinkList/index.js
@@ -1,36 +1,33 @@
 /**
-*
-* LinkList
-*
-*/
+ *
+ * LinkList
+ *
+ */
 
-import React from 'react';
-import styles from './styles.css';
-import Link from '../Link';
+import React from "react";
+import styles from "./styles.css";
+import Link from "../Link";
 
 function LinkList({ links, topicName }) {
-  const linkNodes = links.map(l => (
-    <Link
-      key={l.id}
-      link={l}
-    />
-  ));
+  const linkNodes = links.map(l => <Link key={l.id} link={l} />);
 
   return (
     <div className={styles.linkList}>
-      {topicName}
+      <h1>{topicName}</h1>
       {linkNodes}
     </div>
   );
 }
 
 LinkList.propTypes = {
-  links: React.PropTypes.arrayOf(React.PropTypes.shape({
-    description: React.PropTypes.string.isRequired,
-    url: React.PropTypes.string.isRequired,
-    id: React.PropTypes.string.isRequired,
-  })),
-  topicName: React.PropTypes.string.isRequired,
+  links: React.PropTypes.arrayOf(
+    React.PropTypes.shape({
+      description: React.PropTypes.string.isRequired,
+      url: React.PropTypes.string.isRequired,
+      id: React.PropTypes.string.isRequired
+    })
+  ),
+  topicName: React.PropTypes.string.isRequired
 };
 
 export default LinkList;
