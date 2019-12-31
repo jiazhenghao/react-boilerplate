c276e44255073d711ffb675154d4aec25889a15a
Zhenghao Jia <cherie-xia@hotmail.com>
2019-12-31 14:07:37 +0800

    icon button

--- app/components/LinkList/index.js
+++ app/components/LinkList/index.js
@@ -1,35 +1,39 @@
-/* eslint-disable comma-dangle */
-/* eslint-disable quotes */
 /**
- *
- * LinkList
- *
- */
+*
+* LinkList
+*
+*/
 
-import React from "react";
-import styles from "./styles.css";
-import Link from "../Link";
+import React from 'react';
 
-function LinkList({ links, topicName }) {
-  const linkNodes = links.map(l => <Link key={l.id} link={l} />);
+import Link from '../Link';
+import styles from './styles.css';
+
+function LinkList({ links, topicName, children }) {
+  const linkNodes = links.map(l => (
+    <Link
+      key={l.id}
+      link={l}
+    />
+  ));
 
   return (
     <div className={styles.linkList}>
       <h1>{topicName}</h1>
       {linkNodes}
+      {children}
     </div>
   );
 }
 
 LinkList.propTypes = {
-  links: React.PropTypes.arrayOf(
-    React.PropTypes.shape({
-      description: React.PropTypes.string.isRequired,
-      url: React.PropTypes.string.isRequired,
-      id: React.PropTypes.string.isRequired
-    })
-  ),
-  topicName: React.PropTypes.string.isRequired
+  children: React.PropTypes.element.isRequired,
+  topicName: React.PropTypes.string.isRequired,
+  links: React.PropTypes.arrayOf(React.PropTypes.shape({
+    description: React.PropTypes.string.isRequired,
+    url: React.PropTypes.string.isRequired,
+    id: React.PropTypes.string.isRequired,
+  })),
 };
 
 export default LinkList;
